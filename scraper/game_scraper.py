92b0913808c3647a62a51f49b1ddeaf31836f1d1
root <admin@example.com>
2017-08-26 00:45:44 +0900

    [add]game_scraper

---
+++ scraper/game_scraper.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import texttable as ttb
+from scraper.scraper import Scraper
+from utils import Utils
+
+__author__ = 'prs-watch'
+
+class GScraper(Scraper):
+	def scrape(self):
+		"""
+		scrape game info
+		"""
+		base_url = self.DELIMITER.join([
+				self.BASE_URL
+			,	self.YMD_FORMAT.format(**self.params)
+			,	'epg.xml'
+			])
+		html = Utils.get_content(base_url,self.PARSER)
+		games = Utils.find_all_tags(html,'game')
+
+		table = ttb.Texttable()
+		table.set_deco(ttb.Texttable.HEADER)
+		table.set_cols_align(['c','c','c','c','c'])
+
+		table_contents = []
+		table_contents.append([
+				'NO'
+			,	'VENUE'
+			,	'HOME_TEAM'
+			,	'AWAY_TEAM'
+			,	'RESULT'
+			])
+
+		no = 0
+
+		for game in games:
+
+			no += 1
+
+			content = []
+
+			venue = Utils.find_attr(game,'venue')
+			away_team_name = Utils.find_attr(game,'away_team_name')
+			home_team_name = Utils.find_attr(game,'home_team_name')
+			away_win = Utils.find_attr(game,'away_win')
+			away_loss = Utils.find_attr(game,'away_loss')
+			home_win = Utils.find_attr(game,'home_win')
+			home_loss = Utils.find_attr(game,'home_loss')
+			away_team_runs = Utils.find_attr(game,'away_team_runs')
+			home_team_runs = Utils.find_attr(game,'home_team_runs')
+
+			away = away_team_name + '(' + away_win + '-' + away_loss + ')'
+			home = home_team_name + '(' + home_win + '-' + home_loss + ')'
+			result = away_team_runs + '-' + home_team_runs
+
+			content.append(str(no))
+			content.append(venue)
+			content.append(away)
+			content.append(home)
+			content.append(result)
+
+			table_contents.append(content)
+
+		table.add_rows(table_contents)
+		print (table.draw())
