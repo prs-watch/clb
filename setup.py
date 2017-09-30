6198bf32ba0d8c22b08d2dce316cf1e63dd27755
root <admin@example.com>
2017-09-30 15:18:31 +0900

    [fix]

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='commandline-baseball',
-    version='0.1.0',
+    version='1.0.0',
     description='Access to MLB game informations on terminal',
     author='prs-watch',
     author_email='prs_watch@excite.co.jp',
     url='https://github.com/prs-watch/commandline-baseball',
-    packages=find_packages(),
+    packages=['clb','clb.scraper'],
     license='MIT License',
     include_package_data=True,
     install_requires=open('requirements.txt').read().splitlines(),
